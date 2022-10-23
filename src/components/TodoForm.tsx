251d88ab2d29e1b03cee1eb9400be8ab41532b1a
becarrillo <77902818+becarrillo@users.noreply.github.com>
2022-10-22 23:01:55 -0500

    Update TodoForm

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,7 +1,18 @@
 const TodoForm = () => {
     
     return (
-        <div>Hello TodoForm!</div>
+        <div id="query-adv" className="modal bg-cyan-50 shadow-xl shadow-air-super-blue">
+            <form action="/form" method="get" className="flex flex-col px-2 py-12 grid rounded-lg grid-rows-2 mx-auto">
+                <div className="grid grid-cols-2 gap-10 h-32 ">
+                    <label htmlFor="#your_select" className="col-span-2 mt-4 text-center text-lg">Selecciona el método para crear tarea(s)/actividad(es)</label>
+                    <select name="select_opt" className="col-span-2 rounded-md bg-white shadow-md shadow-zinc-400 mx-auto w-64 h-8">
+                        <option value="descripcion manual" >descripción manual</option>
+                        <option value="de la API de comentarios">de la API de comentarios</option>
+                    </select>
+                    <input type="submit" className="col-span-2 rounded-sm bg-cyan-700 text-white cursor-pointer mx-auto px-28 py-1 hover:bg-cyan-600" value="Comenzar" />
+                </div>
+            </form>
+        </div>
     )
 }
 
