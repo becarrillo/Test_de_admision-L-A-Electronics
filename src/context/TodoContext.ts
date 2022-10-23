89c50dba7b483faf9dbd4f87b755506ac1d3b90c
becarrillo <77902818+becarrillo@users.noreply.github.com>
2022-10-22 23:08:28 -0500

    Update TodoContext

--- src/context/TodoContext.ts
+++ src/context/TodoContext.ts
@@ -1,4 +1,10 @@
 import { createContext } from "react";
 
 
-export const TodoContext = createContext({});
+export const TodoContext = createContext({
+    to_navigate_forms: true,
+    tasks: {
+        description: '',
+        state: ''
+    }
+});
